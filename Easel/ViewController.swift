c4c8b1525b94fc8a359078d1dbb373849085ea3c
Patrick <patrickbellot77@gmail.com>
2017-08-21 13:56:59 -0500

    add touches and notification

--- Easel/ViewController.swift
+++ Easel/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var buttonStackView: UIStackView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    NotificationCenter.default.addObserver(self, selector: #selector(ViewController.appBecameActive), name: NSNotification.Name.UIApplicationDidBecomeActive , object: nil)
   }
   
   override func viewDidAppear(_ animated: Bool) {
     self.navigationController?.navigationBar.isHidden = true
   }
+  
+  func appBecameActive() {
+    self.buttonStackView.isHidden = false
+  }
+
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    print("Began")
+    self.buttonStackView.isHidden = true
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    print("Moved")
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    self.buttonStackView.isHidden = false
+    print("Ended")
+  }
 
 
-
-}
-
+} // End of class

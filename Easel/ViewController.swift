622ecf07195df8d8cc27b4a2aee10f063c316296
Patrick <patrickbellot77@gmail.com>
2017-08-21 14:38:09 -0500

    add path and code depreciation

--- Easel/ViewController.swift
+++ Easel/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
   @IBOutlet weak var buttonStackView: UIStackView!
+  @IBOutlet weak var imageView: UIImageView!
+  
+  var lastPoint = CGPoint.zero
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -27,12 +30,28 @@ class ViewController: UIViewController {
   }
 
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print("Began")
+    if let touch = touches.first {
+      let point = touch.location(in: self.imageView)
+      self.lastPoint = point
+    }
+    
     self.buttonStackView.isHidden = true
   }
   
   override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print("Moved")
+    if let touch = touches.first {
+      let point = touch.location(in: self.imageView)
+      print(point)
+    }
+    
+    UIGraphicsBeginImageContext(self.imageView.frame.size)
+    let context = UIGraphicsGetCurrentContext()
+    
+    self.imageView.image?.draw(in: CGRect(x: 0, y: 0, width: self.imageView.frame.size.width, height: self.imageView.frame.size.height))
+    
+//    CGContext.move(context, self.lastPoint.x, self.lastPoint.y) // Depricated Context roadblock
+    
+    UIGraphicsEndImageContext()
   }
   
   override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {

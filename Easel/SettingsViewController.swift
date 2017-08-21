1bf70c37079d413a8229b708bb4cb1f7183ca3d6
Patrick <patrickbellot77@gmail.com>
2017-08-21 13:25:41 -0500

    initial commit and setup UI

---
+++ Easel/SettingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.swift
+//  Easel
+//
+//  Created by Patrick Bellot on 8/21/17.
+//  Copyright © 2017 Polestar Interactive LLC. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.navigationBar.isHidden = false
+    }
+
+
+
+}
--- Easel/ViewController.swift
+++ Easel/ViewController.swift
@@ -12,13 +12,13 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    self.navigationController?.navigationBar.isHidden = true
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
 
 
 }
